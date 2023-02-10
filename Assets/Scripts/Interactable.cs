c42e81527a5015d41c47f80d603ba1d9aa6f8c82
ariesps2005 <120816599+ariesps2005@users.noreply.github.com>
2023-02-10 14:49:46 +0300

    Spawning

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ziggurat;
 
 [RequireComponent(typeof(Outline))]
-public class Interactable : MonoBehaviour
+public class Interactable : UnitCreator
 {
     private Outline outline;
 
     private void OnEnable()
     {
         outline = GetComponent<Outline>();
-        outline.OutlineWidth = 0;
+        outline.OutlineWidth = 0;      
     }
     public void OnHoverEnter()
     {
         outline.OutlineWidth = 10;
+        
+        InstantiateKnightUnit();      
     }
     public void OnHoverExit()
     {
