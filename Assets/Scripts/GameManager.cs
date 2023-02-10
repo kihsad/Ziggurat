af53b932c54b46e30127e0a3b4370faf6a0ab471
ariesps2005 <120816599+ariesps2005@users.noreply.github.com>
2023-02-10 14:40:56 +0300

    Knights Spawning

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ziggurat;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject _instanceUnit; 
+}
---
+++ Assets/Scripts/UnitCreator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Ziggurat
+{
+    public class UnitCreator : MonoBehaviour
+    {
+        [SerializeField] GameObject unitPrefab;
+
+        [Header("Configs")]
+        [SerializeField] private float _moveSpeed = 1f;
+        [SerializeField] private int _health = 10;
+        [SerializeField] private int _lightDamage = 1;
+        [SerializeField] private int _heavyDamage = 3;
+        [SerializeField] private float _missAbility = 0.5f;
+        
+      
+
+        public void InstantiateKnightUnit()
+        {
+            GameObject KnightUnit = Instantiate(unitPrefab);
+
+            KnightUnit.transform.position = new Vector3(transform.position.x, transform.position.y + 3f, transform.position.z);
+                        
+            Debug.Log("Unit Knight is on!");
+        }
+
+        
+
+    }
+}
